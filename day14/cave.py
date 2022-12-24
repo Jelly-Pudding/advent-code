08f38adaa2e96f4df93c1c3bcaea648a066eefa8
Jelly-Pudding <samwalters11111@hotmail.com>
2022-12-24 22:06:47 +0000

    chatgpt does day14 part 1

--- day14/cave.py
+++ day14/cave.py
@@ -6,6 +6,7 @@ with open("input.txt", "r") as f:
         lst.append(line.strip())
 
 def draw_cave(paths):
+  lol = []
   # Create a map of the cave
   cave = defaultdict(lambda: '.')
   
@@ -44,8 +45,49 @@ def draw_cave(paths):
   # Add each row of the cave to the result string
   for y in range(min_y, max_y + 1):
     result += ''.join(cave[(x, y)] for x in range(min_x, max_x + 1)) + '\n'
+    lol.append(''.join(cave[(x, y)] for x in range(min_x, max_x + 1)))
   
-  return result
+  return result, lol
 
-print(draw_cave(lst))
+def simulate(state):
+    # Convert the state to a list of lists
+    state = [list(row) for row in state]
 
+    # Find the starting position of the o
+    start_row, start_col = None, None
+    for i, row in enumerate(state):
+        if '+' in row:
+            start_row = i
+            start_col = row.index('+')
+            break
+    # Initialize the current position of the o and the rest flag
+    cur_row, cur_col = start_row, start_col
+    counter = 0
+    while True:
+        # Keep moving the o down until it can't go any further
+        while cur_row < len(state) - 1 and state[cur_row + 1][cur_col] == '.':
+            cur_row += 1
+
+        # Try to move it diagonally.
+        if cur_row < len(state) - 1:
+            # Try to move the o diagonally to the left.
+            if cur_col > 0 and state[cur_row + 1][cur_col - 1] == '.':
+                cur_row += 1
+                cur_col -= 1
+                continue
+            # If that's not possible, try to move the o diagonally to the right.
+            elif cur_col < len(state[0]) - 1 and state[cur_row + 1][cur_col + 1] == '.':
+                cur_row += 1
+                cur_col += 1
+                continue
+            # Out of bounds stuff now.
+            elif cur_col == 0 or cur_col == len(state[0]) - 1:
+                break
+            else:
+                state[cur_row][cur_col] = "o"
+                counter += 1
+                cur_row, cur_col = start_row, start_col
+    return state, counter
+
+cave = draw_cave(lst)[1]
+print(simulate(cave)[1])
